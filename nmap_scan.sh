ac0203888d1d9afe6d544fd20012c1b077742f11
Iliya Gorshkov <i.gorshkov@akytec.rs>
2025-08-07 17:35:31 +0200

    script: add condition if already installed

--- nmap_scan.sh
+++ nmap_scan.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "📥 Downloading nmap package..."
-curl -sL -o /tmp/nmap_7.80-r0_aarch64.ipk https://github.com/iliya35/nmap/raw/refs/heads/main/nmap_7.80-r0_aarch64.ipk
+if command -v nmap >/dev/null 2>&1; then
+  echo "✅ nmap is already installed. Skipping installation."
+else
+  echo "📥 Downloading nmap package..."
+  curl -sL -o /tmp/nmap_7.80-r0_aarch64.ipk https://github.com/iliya35/nmap/raw/refs/heads/main/nmap_7.80-r0_aarch64.ipk
 
-echo "📦 Installing nmap..."
-opkg install /tmp/nmap_7.80-r0_aarch64.ipk
+  echo "📦 Installing nmap..."
+  opkg install /tmp/nmap_7.80-r0_aarch64.ipk
+fi
 
 echo "🔍 Scanning port 26502 on gate.akytec.com:"
-nmap -p 26502 gate.akytec.com || echo "Scan failed for gate.akytec.com"
+nmap -p 26502 gate.akytec.com || echo "❌ Scan failed for gate.akytec.com"
 
 echo "\n🔍 Scanning port 26502 on cloud.akytec.de:"
-nmap -p 26502 cloud.akytec.de || echo "Scan failed for cloud.akytec.de"
+nmap -p 26502 cloud.akytec.de || echo "❌ Scan failed for cloud.akytec.de"
 
 echo "\n✅ Done."
 
